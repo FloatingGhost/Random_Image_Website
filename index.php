eceb21183894c51248c99ebbb32432e2f959ec77
Hannah Ward <Hannah.ward9001@gmail.com>
2016-02-29 23:54:51 +0000

    init

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>Random X generator</title>
+  </head>
+
+  <h1>Random X generator!</h1>
+  <form method="post" action="create.php">
+    <label for="name">Add new random generator:</label>
+    <input type="text" name="name"/>
+    <button type="submit">Create!</button>
+  </form>  
+</html>
